3eb1a9a1a7de3d877abd705f311e9050ab2bb30e
David Ewan Campbell <ewan.david@gmail.com>
2023-04-21 17:02:33 +0100

    Test changed to queryByTestId which looks for a match with the given ID prop and throws an error if matching element not found, as opposed to getByTestId

--- src/Counter.test.tsx
+++ src/Counter.test.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { render } from "@testing-library/react-native";
+import { fireEvent, render } from "@testing-library/react-native";
 
 import Counter from "./Counter";
 
@@ -11,12 +11,25 @@ describe("Counter", () => {
 
   it("shows an initial state of 0", () => {
     const { queryByText } = render(<Counter />);
-
+    // queryByText gets a child component that matches a specific test
     const initialState = queryByText("Counter: 0");
     expect(initialState).not.toBeNull();
   });
 
-  it.todo("increments by 1 each time increment is pressed");
+  it("increments by 1 each time increment is pressed", () => {
+    const { queryByText, queryByTestId } = render(<Counter />);
+    // changed to queryByTestId - looks for a component that matches the given test ID props - returning null instead
+    const initialState = queryByText("Counter: 0");
+    expect(initialState).not.toBeNull();
+
+    const increment = queryByTestId("incrementButton");
+    expect(increment).not.toBeNull();
+
+    fireEvent.press(increment);
+
+    const oneState = queryByText("Counter: 1");
+    expect(oneState).not.toBeNull();
+  });
 
   it.todo("decrements by 1 each time decrement is pressed");
 
