93badad5ab86e3ffd420498ab46f49e1711709a1
David Ewan Campbell <ewan.david@gmail.com>
2023-04-21 16:12:25 +0100

    Desired tests added for app but on hold with todo and will write each test as I progress

--- src/Counter.test.tsx
+++ src/Counter.test.tsx
@@ -3,3 +3,17 @@ describe("Counter", () => {
       expect(1 + 1).toEqual(2);
     });
   });
+
+it.todo("renders correctly");
+
+it.todo("shows an initial state of 0");
+
+it.todo("increments by 1 each time increment is pressed");
+
+it.todo("decrements by 1 each time decrement is pressed");
+
+// I want the app to...
+// Render correctly
+// Show an initial state of zero counter
+// Increment counter by one each time increment button is pressed
+// Decrement counter by one each time decrement button is pressed
