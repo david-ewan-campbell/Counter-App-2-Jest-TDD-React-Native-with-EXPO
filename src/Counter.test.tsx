2a141512204d639baf05ac1edf9097ad963963cb
David Ewan Campbell <ewan.david@gmail.com>
2023-04-21 16:24:52 +0100

    Created a counter component so that test to check that it is rendering passes

--- src/Counter.test.tsx
+++ src/Counter.test.tsx
@@ -1,16 +1,21 @@
+import React from "react";
+
+import { render } from "@testing-library/react-native";
+
+import Counter from "./Counter";
+
 describe("Counter", () => {
-    it("1 + 1 should equals 2", () => {
-      expect(1 + 1).toEqual(2);
-    });
+  it("renders correctly", () => {
+    render(<Counter />);
   });
 
-it.todo("renders correctly");
+  it.todo("shows an initial state of 0");
 
-it.todo("shows an initial state of 0");
+  it.todo("increments by 1 each time increment is pressed");
 
-it.todo("increments by 1 each time increment is pressed");
+  it.todo("decrements by 1 each time decrement is pressed");
 
-it.todo("decrements by 1 each time decrement is pressed");
+});
 
 // I want the app to...
 // Render correctly
